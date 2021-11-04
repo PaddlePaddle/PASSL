2de8245f6e3c684fbdf78f621c9a22f0d4d88207
duanboqiang <firestonelib@gmail.com>
2021-11-04 21:56:33 +0800

    Header Fix (#39)
    * add swin transformer
    
    * add swin transformer
    
    * add swin transformer
    
    * header fix
    
    * add license
    
    * fix license

--- passl/utils/registry.py
+++ passl/utils/registry.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
+# Copyright (c) 2017-present, Facebook, Inc. All rights reserved.
+# copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
+#
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+#Taken and modified for PASSL from:
+#    https://github.com/facebookresearch/fvcore/blob/main/fvcore/common/registry.py
+#
 
 import inspect
 import traceback
