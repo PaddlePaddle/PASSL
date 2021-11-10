98fac9a7c0b25803cb2dfe162649a81ae1a54077
Mike <60082702+lmk123568@users.noreply.github.com>
2021-11-10 15:04:48 +0800

    add quote

--- passl/modeling/backbones/beit.py
+++ passl/modeling/backbones/beit.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Code was based on https://github.com/microsoft/unilm/tree/master/beit
+
 import copy
 import math
 from functools import partial
