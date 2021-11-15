252036ace298d8d54df318ce3088c4c2438b19b8
duanboqiang <firestonelib@gmail.com>
2021-11-15 16:16:54 +0800

    Distributed Launch Fix (#52)
    * add swin transformer
    
    * add swin transformer
    
    * add swin transformer
    
    * modify simclr docs

--- passl/modules/__init__.py
+++ passl/modules/__init__.py
@@ -12,5 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .ddp import DistributedDataParallel
 from .freeze import freeze_batchnorm_statictis
