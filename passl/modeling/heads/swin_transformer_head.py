e0c4a4add615c20a26b6e0d4c211ae0320334e5c
duanboqiang <firestonelib@gmail.com>
2021-11-24 19:23:16 +0800

    Swin Head (#62)
    * add swin transformer
    
    * add swin transformer
    
    * add swin transformer
    
    * add swin head

--- passl/modeling/heads/swin_transformer_head.py
+++ passl/modeling/heads/swin_transformer_head.py
@@ -14,6 +14,7 @@
 
 import paddle
 import paddle.nn as nn
+import paddle.nn.functional as F
 
 from ...modules.init import reset_parameters, normal_init
 from .builder import HEADS
@@ -22,43 +23,38 @@ from .clas_head import ClasHead
 
 @HEADS.register()
 class SwinTransformerClsHead(ClasHead):
-    """Vision Transformer classifier head.
+    """Swin Transformer classifier head.
 
     Args:
         with_avg_pool (bool): Use average pooling or not. Default: False.
         in_channels (int): Number of channels in the input feature map.
         num_classes (int): Number of categories excluding the background
-            category. 
+            category.
     """
-    def __init__(self,
-                 with_avg_pool=False,
-                 in_channels=2048,
-                 num_classes=1000):
+    def __init__(self, with_avg_pool=False, in_channels=2048, num_classes=1000):
         super(SwinTransformerClsHead, self).__init__()
-        self.with_avg_pool = with_avg_pool 
+        self.with_avg_pool = with_avg_pool
         self.in_channels = in_channels
         self.num_classes = num_classes
-        self.criterion = nn.CrossEntropyLoss()
         self.fc_cls = nn.Linear(in_channels, num_classes)
+        if self.with_avg_pool:
+            self.avg_pool = nn.AdaptiveAvgPool1D(1)
 
         normal_init(self.fc_cls, mean=0.0, std=0.01, bias=0.0)
 
     def forward(self, x):
         if self.with_avg_pool:
-            assert x.dim() == 4, \
-                "Tensor must has 4 dims, got: {}".format(x.dim())
             x = self.avg_pool(x)
         x = paddle.flatten(x, 1)
         cls_score = self.fc_cls(x)
         return cls_score
 
-    def loss(self, cls_score, labels):
+    def loss(self, x, labels):
         losses = dict()
 
-        losses['loss'] = self.criterion(cls_score, labels)
-        losses['acc1'], losses['acc5'] = accuracy(cls_score,
-                                                  labels,
-                                                  topk=(1, 5))
+        losses['loss'] = paddle.sum(-labels * F.log_softmax(x, axis=-1),
+                                    axis=-1).mean()
+        losses['acc1'], losses['acc5'] = accuracy(x, labels, topk=(1, 5))
         return losses
 
 
@@ -66,6 +62,8 @@ def accuracy(output, target, topk=(1, )):
     """Computes the accuracy over the k top predictions for the specified values of k"""
     with paddle.no_grad():
         maxk = max(topk)
+        if target.dim() > 1:
+            target = target.argmax(axis=-1)
         batch_size = target.shape[0]
 
         _, pred = output.topk(maxk, 1, True, True)
