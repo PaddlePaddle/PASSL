fc551f9c715ebf3dd32f06912d4c5d79e9106cf2
firestonelib <firestonelib@gmail.com>
2021-11-24 13:36:45 +0800

    fixes vit bias bug

--- passl/modeling/backbones/vision_transformer.py
+++ passl/modeling/backbones/vision_transformer.py
@@ -36,7 +36,7 @@ def drop_path(x, drop_prob=0.0, training=False):
     if drop_prob == 0.0 or not training:
         return x
     keep_prob = paddle.to_tensor(1 - drop_prob)
-    shape = (paddle.shape(x)[0],) + (1,) * (x.ndim - 1)
+    shape = (paddle.shape(x)[0], ) + (1, ) * (x.ndim - 1)
     random_tensor = keep_prob + paddle.rand(shape, dtype=x.dtype)
     random_tensor = paddle.floor(random_tensor)  # binarize
     output = x.divide(keep_prob) * random_tensor
@@ -45,7 +45,6 @@ def drop_path(x, drop_prob=0.0, training=False):
 
 class DropPath(nn.Layer):
     """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)."""
-
     def __init__(self, drop_prob=None):
         super(DropPath, self).__init__()
         self.drop_prob = drop_prob
@@ -102,7 +101,7 @@ class Attention(nn.Layer):
         super().__init__()
         self.num_heads = num_heads
         head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim ** -0.5
+        self.scale = qk_scale or head_dim**-0.5
 
         self.qkv = nn.Linear(dim, dim * 3, bias_attr=qkv_bias)
         self.attn_mask = attn_mask
@@ -113,11 +112,9 @@ class Attention(nn.Layer):
     def forward(self, x):
         # B= paddle.shape(x)[0]
         N, C = x.shape[1:]
-        qkv = (
-            self.qkv(x)
-            .reshape((-1, N, 3, self.num_heads, C // self.num_heads))
-            .transpose((2, 0, 3, 1, 4))
-        )
+        qkv = (self.qkv(x).reshape(
+            (-1, N, 3, self.num_heads, C // self.num_heads)).transpose(
+                (2, 0, 3, 1, 4)))
         q, k, v = qkv[0], qkv[1], qkv[2]
         attn = (q.matmul(k.transpose((0, 1, 3, 2)))) * self.scale
         if self.attn_mask is not None:
@@ -181,44 +178,39 @@ class Block(nn.Layer):
 
 
 class Transformer(nn.Layer):
-    def __init__(
-        self,
-        embed_dim=768,
-        depth=12,
-        num_heads=12,
-        mlp_ratio=4,
-        qkv_bias=True,
-        qk_scale=None,
-        drop_rate=0.0,
-        attn_mask=None,
-        attn_drop_rate=0.0,
-        drop_path_rate=0.0,
-        norm_layer="nn.LayerNorm",
-        epsilon=1e-5,
-        **args
-    ):
+    def __init__(self,
+                 embed_dim=768,
+                 depth=12,
+                 num_heads=12,
+                 mlp_ratio=4,
+                 qkv_bias=True,
+                 qk_scale=None,
+                 drop_rate=0.0,
+                 attn_mask=None,
+                 attn_drop_rate=0.0,
+                 drop_path_rate=0.0,
+                 norm_layer="nn.LayerNorm",
+                 epsilon=1e-5,
+                 **args):
         super().__init__()
         self.embed_dim = embed_dim
         self.depth = depth
         dpr = np.linspace(0, drop_path_rate, depth)
-        self.blocks = nn.LayerList(
-            [
-                Block(
-                    dim=embed_dim,
-                    num_heads=num_heads,
-                    mlp_ratio=mlp_ratio,
-                    qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
-                    drop=drop_rate,
-                    attn_mask=attn_mask,
-                    attn_drop=attn_drop_rate,
-                    drop_path=dpr[i],
-                    norm_layer=norm_layer,
-                    epsilon=epsilon,
-                )
-                for i in range(depth)
-            ]
-        )
+        self.blocks = nn.LayerList([
+            Block(
+                dim=embed_dim,
+                num_heads=num_heads,
+                mlp_ratio=mlp_ratio,
+                qkv_bias=qkv_bias,
+                qk_scale=qk_scale,
+                drop=drop_rate,
+                attn_mask=attn_mask,
+                attn_drop=attn_drop_rate,
+                drop_path=dpr[i],
+                norm_layer=norm_layer,
+                epsilon=epsilon,
+            ) for i in range(depth)
+        ])
 
     def forward(self, x):
         for blk in self.blocks:
@@ -228,26 +220,30 @@ class Transformer(nn.Layer):
 
 class PatchEmbed(nn.Layer):
     """Image to Patch Embedding"""
-
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768, bias=True):
+    def __init__(self,
+                 img_size=224,
+                 patch_size=16,
+                 in_chans=3,
+                 embed_dim=768,
+                 patch_bias=True):
         super().__init__()
+        num_patches = (img_size // patch_size) * (img_size // patch_size)
+        self.patches_resolution = (
+            (img_size // patch_size),
+            (img_size // patch_size),
+        )
         img_size = (img_size, img_size)
         patch_size = (img_size, patch_size)
-        num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0])
         self.img_size = img_size
         self.patch_size = patch_size
-        self.patches_resolution = (
-            (img_size[1] // patch_size[1]),
-            (img_size[0] // patch_size[0]),
-        )
         self.num_patches = num_patches
 
         self.proj = nn.Conv2D(
             in_chans,
             embed_dim,
-            kernel_size=patch_size,
-            stride=patch_size,
-            bias_attr=bias,
+            kernel_size=patch_size[0],
+            stride=patch_size[0],
+            bias_attr=patch_bias,
         )
 
     def forward(self, x):
@@ -263,55 +259,49 @@ class PatchEmbed(nn.Layer):
 @BACKBONES.register()
 class VisionTransformer(nn.Layer):
     """Vision Transformer with support for patch input"""
-
-    def __init__(
-        self,
-        img_size=224,
-        patch_size=16,
-        in_chans=3,
-        class_dim=0,
-        width=768,
-        out_dim=512,
-        depth=12,
-        num_heads=12,
-        mlp_ratio=4,
-        qkv_bias=True,
-        qk_scale=None,
-        drop_rate=0.0,
-        attn_drop_rate=0.0,
-        drop_path_rate=0.0,
-        norm_layer="nn.LayerNorm",
-        pre_norm=False,
-        proj=False,
-        output_cls_token=True,
-        patch_bias=True,
-        epsilon=1e-5,
-        **args
-    ):
+    def __init__(self,
+                 img_size=224,
+                 patch_size=16,
+                 in_chans=3,
+                 class_dim=0,
+                 width=768,
+                 out_dim=512,
+                 depth=12,
+                 num_heads=12,
+                 mlp_ratio=4,
+                 qkv_bias=True,
+                 qk_scale=None,
+                 drop_rate=0.0,
+                 attn_drop_rate=0.0,
+                 drop_path_rate=0.0,
+                 norm_layer="nn.LayerNorm",
+                 pre_norm=False,
+                 proj=False,
+                 output_cls_token=True,
+                 patch_bias=True,
+                 epsilon=1e-5,
+                 **args):
         super().__init__()
         self.class_dim = class_dim
 
         self.num_features = self.width = width
 
-        self.patch_embed = PatchEmbed(
-            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=width, patch_bias=patch_bias
-        )
+        self.patch_embed = PatchEmbed(img_size=img_size,
+                                      patch_size=patch_size,
+                                      in_chans=in_chans,
+                                      embed_dim=width,
+                                      patch_bias=patch_bias)
         num_patches = self.patch_embed.num_patches
 
-        scale = width ** -0.5
+        scale = width**-0.5
         self.class_embedding = self.create_parameter(
-            shape=(1, 1, width), default_initializer=Normal(std=scale)
-        )
+            shape=(1, 1, width), default_initializer=Normal(std=scale))
         self.positional_embedding = self.create_parameter(
-            shape=(1, num_patches + 1, width), default_initializer=Normal(std=scale)
-        )
-        self.proj = (
-            self.create_parameter(
-                shape=(width, out_dim), default_initializer=Normal(std=scale)
-            )
-            if proj
-            else None
-        )
+            shape=(1, num_patches + 1, width),
+            default_initializer=Normal(std=scale))
+        self.proj = (self.create_parameter(shape=(width, out_dim),
+                                           default_initializer=Normal(
+                                               std=scale)) if proj else None)
         self.add_parameter("positional_embedding", self.positional_embedding)
         self.add_parameter("class_embedding", self.class_embedding)
         self.pos_drop = nn.Dropout(p=drop_rate)
@@ -319,27 +309,23 @@ class VisionTransformer(nn.Layer):
 
         dpr = np.linspace(0, drop_path_rate, depth)
 
-        self.norm_pre = (
-            eval(norm_layer)(width, epsilon=epsilon) if pre_norm else Identity()
-        )
+        self.norm_pre = (eval(norm_layer)(width, epsilon=epsilon)
+                         if pre_norm else Identity())
 
-        self.blocks = nn.LayerList(
-            [
-                Block(
-                    dim=width,
-                    num_heads=num_heads,
-                    mlp_ratio=mlp_ratio,
-                    qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
-                    drop=drop_rate,
-                    attn_drop=attn_drop_rate,
-                    drop_path=dpr[i],
-                    norm_layer=norm_layer,
-                    epsilon=epsilon,
-                )
-                for i in range(depth)
-            ]
-        )
+        self.blocks = nn.LayerList([
+            Block(
+                dim=width,
+                num_heads=num_heads,
+                mlp_ratio=mlp_ratio,
+                qkv_bias=qkv_bias,
+                qk_scale=qk_scale,
+                drop=drop_rate,
+                attn_drop=attn_drop_rate,
+                drop_path=dpr[i],
+                norm_layer=norm_layer,
+                epsilon=epsilon,
+            ) for i in range(depth)
+        ])
 
         self.norm_post = eval(norm_layer)(width, epsilon=epsilon)
 
