ed4c99519d03b17e51d9e1f3fad56e43b43d9883
firestonelib <firestonelib@gmail.com>
2022-02-24 15:19:23 +0800

    add beit

--- passl/datasets/preprocess/transforms.py
+++ passl/datasets/preprocess/transforms.py
@@ -455,3 +455,113 @@ class NormalizeImage(PT.Normalize):
         img = F.normalize(img, self.mean, self.std, self.data_format,
                           self.to_rgb)
         return img.astype(self.dtype)
+
+
+@TRANSFORMS.register()
+class RandomResizedCropAndInterpolationWithTwoPic(PT.RandomResizedCrop):
+    """Crop the given PIL Image to random size and aspect ratio with random interpolation.
+    A crop of random size (default: of 0.08 to 1.0) of the original size and a random
+    aspect ratio (default: of 3/4 to 4/3) of the original aspect ratio is made.
+    This crop is finally resized to given size.
+    This is popularly used to train the Inception networks.
+
+    Args:
+        size: expected output size of each edge
+        second size: second expected output size of each edge
+        scale: range of size of the origin size cropped
+        ratio: range of aspect ratio of the origin aspect ratio cropped
+        interpolation: Default: PIL.Image.BILINEAR
+        second_interpolation: Default: PIL.Image.LANCZOS
+    """
+    def __init__(self,
+                 size,
+                 second_size=None,
+                 scale=(0.08, 1.0),
+                 ratio=(3. / 4., 4. / 3.),
+                 interpolation='bilinear',
+                 second_interpolation='lanczos',
+                 keys=None):
+        super(RandomResizedCropAndInterpolationWithTwoPic, self).__init__(keys)
+        if isinstance(size, list):
+            self.size = size
+        else:
+            self.size = [size, size]
+        if second_size is not None:
+            if isinstance(second_size, list):
+                self.second_size = second_size
+            else:
+                self.second_size = [second_size, second_size]
+        else:
+            self.second_size = None
+        if (scale[0] > scale[1]) or (ratio[0] > ratio[1]):
+            warnings.warn("range should be of kind (min, max)")
+
+        if interpolation == 'random':
+            self.interpolation = _RANDOM_INTERPOLATION
+        else:
+            self.interpolation = interpolation
+        self.second_interpolation = second_interpolation
+        self.scale = scale
+        self.ratio = ratio
+
+    def get_params(self, img, scale, ratio):
+        """Get parameters for ``crop`` for a random sized crop.
+
+        Args:
+            img (PIL Image): Image to be cropped.
+            scale (tuple): range of size of the origin size cropped
+            ratio (tuple): range of aspect ratio of the origin aspect ratio cropped
+
+        Returns:
+            tuple: params (i, j, h, w) to be passed to ``crop`` for a random
+                sized crop.
+        """
+        area = img.size[0] * img.size[1]
+
+        for attempt in range(10):
+            target_area = random.uniform(*scale) * area
+            log_ratio = (math.log(ratio[0]), math.log(ratio[1]))
+            aspect_ratio = math.exp(random.uniform(*log_ratio))
+
+            w = int(round(math.sqrt(target_area * aspect_ratio)))
+            h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if w <= img.size[0] and h <= img.size[1]:
+                i = random.randint(0, img.size[1] - h)
+                j = random.randint(0, img.size[0] - w)
+                return i, j, h, w
+
+        # Fallback to central crop
+        in_ratio = img.size[0] / img.size[1]
+        if in_ratio < min(ratio):
+            w = img.size[0]
+            h = int(round(w / min(ratio)))
+        elif in_ratio > max(ratio):
+            h = img.size[1]
+            w = int(round(h * max(ratio)))
+        else:  # whole image
+            w = img.size[0]
+            h = img.size[1]
+        i = (img.size[1] - h) // 2
+        j = (img.size[0] - w) // 2
+        return i, j, h, w
+
+    def _apply_image(self, img):
+        """
+        Args:
+            img (PIL Image): Image to be cropped and resized.
+
+        Returns:
+            PIL Image: Randomly cropped and resized image.
+        """
+        i, j, h, w = self.get_params(img, self.scale, self.ratio)
+        if isinstance(self.interpolation, (tuple, list)):
+            interpolation = random.choice(self.interpolation)
+        else:
+            interpolation = self.interpolation
+        cropped_img = F.crop(img, i, j, h, w)
+        if self.second_size is None:
+            return F.resize(cropped_img, self.size, interpolation)
+        else:
+            return F.resize(img, self.size, interpolation), \
+                   F.resize(img, self.second_size, self.second_interpolation)
