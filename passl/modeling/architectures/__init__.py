702a8f269f7567a88ecb4e7f43fd88c442096c3c
firestonelib <firestonelib@gmail.com>
2022-02-24 14:44:56 +0800

    add device

--- passl/modeling/architectures/__init__.py
+++ passl/modeling/architectures/__init__.py
@@ -12,26 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .moco import MoCo
-from .clas import Classification
-from .BYOL import BYOL
-from .MoCoBYOL import MoCoBYOL
-from .CLIPWrapper import CLIPWrapper
-from .simclr import SimCLR
-from .byol_clas import ByolClassification
-from .ViTWrapper import ViTWrapper
-from .SwinWrapper import SwinWrapper
 from .builder import build_model
-
-from .BeitWrapper import BeitWrapper
-
-from .T2TViTWrapper import T2TViTWrapper
-from .CaiTWrapper import CaiTWrapper
-from .MlpMixerWrapper import MlpMixerWrapper
-from .CvTWrapper import CvTWrapper
-
-from .DeiTWrapper import DeiTWrapper
+from .byol_clas import ByolClassification
+from .clas import Classification
+from .moco import MoCo
+from .simclr import SimCLR
 from .pixpro import PixPro
 
-from .MAE import MAE_PRETRAIN, MAE_FINETUNE
+from .BEiTWrapper import BEiTWrapper, BEiTPTWrapper, BEiTFTWrapper
+from .BYOL import BYOL
+from .CaiTWrapper import CaiTWrapper
+from .CLIPWrapper import CLIPWrapper
+from .CvTWrapper import CvTWrapper
+from .DeiTWrapper import DeiTWrapper
 from .DistillationWrapper import DistillationWrapper
+from .MAE import MAE_PRETRAIN, MAE_FINETUNE
+from .MoCoBYOL import MoCoBYOL
+from .MlpMixerWrapper import MlpMixerWrapper
+from .SwinWrapper import SwinWrapper
+from .T2TViTWrapper import T2TViTWrapper
+from .ViTWrapper import ViTWrapper
