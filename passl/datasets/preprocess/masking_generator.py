b0470d33648220b2ca1f8f77f855819e522c0119
firestonelib <firestonelib@gmail.com>
2022-02-24 16:07:20 +0800

    add beit

--- passl/datasets/preprocess/masking_generator.py
+++ passl/datasets/preprocess/masking_generator.py
@@ -1,5 +1,5 @@
 # --------------------------------------------------------'
-# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,28 +12,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-Originally inspired by impl at https://github.com/zhunzhong07/Random-Erasing, Apache 2.0
-Copyright Zhun Zhong & Liang Zheng
 
-Hacked together by / Copyright 2020 Ross Wightman
-
-Modified by Hangbo Bao, for generating the masked position for visual image transformer
-"""
 # --------------------------------------------------------
 # BEIT: BERT Pre-Training of Image Transformers (https://arxiv.org/abs/2106.08254)
 # Github source: https://github.com/microsoft/unilm/tree/master/beit
 # Copyright (c) 2021 Microsoft
 # Licensed under The MIT License [see LICENSE for details]
-# By Hangbo Bao
-# Based on timm, DINO and DeiT code bases
-# https://github.com/rwightman/pytorch-image-models/tree/master/timm
-# Originally inspired by impl at https://github.com/zhunzhong07/Random-Erasing, Apache 2.0
-# Copyright Zhun Zhong & Liang Zheng
-#
-# Hacked together by / Copyright 2020 Ross Wightman
-#
-# Modified by Hangbo Bao, for generating the masked position for visual image transformer
+
 import random
 import math
 import numpy as np
