6781bccfa9723970c1024849ad9cc1f050126ff8
zhaoqi10 <zhaoqi10@baidu.com>
2023-04-25 21:26:38 +0800

    fix lr adjust method in cae/mae.

--- tasks/ssl/cae/util/lr_sched.py
+++ tasks/ssl/cae/util/lr_sched.py
@@ -28,8 +28,5 @@ def adjust_learning_rate(optimizer, epoch, args):
         lr = args.min_lr + (args.lr - args.min_lr) * 0.5 * \
             (1. + math.cos(math.pi * (epoch - args.warmup_epochs) / (args.epochs - args.warmup_epochs)))
     for param_group in optimizer.param_groups:
-        if "lr_scale" in param_group:
-            param_group["lr"] = lr * param_group["lr_scale"]
-        else:
-            param_group["lr"] = lr
+        param_group["lr"] = lr
     return lr
