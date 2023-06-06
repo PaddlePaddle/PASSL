926c4fb7878e066977543f1fae32bdd75a4537a4
liujie44 <liujie44@baidu.com>
2023-06-06 11:43:10 +0800

    [benchmark] Delete dataset after task completion

--- tests/test_tipc/ssl/mocov3/benchmark_common/prepare.sh
+++ tests/test_tipc/ssl/mocov3/benchmark_common/prepare.sh
@@ -22,6 +22,7 @@ python setup.py develop
 mkdir dataset && cd dataset
 cp -r ${BENCHMARK_ROOT}/models_data_cfs/Paddle_distributed/ILSVRC2012.tgz ./
 tar -zxf ILSVRC2012.tgz
+rm -rf ILSVRC2012.tgz
 cd -
 
 # pretrained
