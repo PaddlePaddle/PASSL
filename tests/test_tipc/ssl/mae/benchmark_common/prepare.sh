e6df1a3377a553f32ac5f218871cf43edcfbbc32
Liujie0926 <44688141+Liujie0926@users.noreply.github.com>
2023-06-07 10:18:38 +0800

    [benchmark] Delete dataset after task completion (#172)

--- tests/test_tipc/ssl/mae/benchmark_common/prepare.sh
+++ tests/test_tipc/ssl/mae/benchmark_common/prepare.sh
@@ -20,6 +20,7 @@ python setup.py develop
 mkdir dataset && cd dataset
 cp -r ${BENCHMARK_ROOT}/models_data_cfs/Paddle_distributed/ILSVRC2012.tgz ./
 tar -zxf ILSVRC2012.tgz
+rm -rf ILSVRC2012.tgz
 cd -
 
 # pretrained
